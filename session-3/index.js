2d6bb20a04bdf9f9bf17ab79331ab3f71bb93cfc
RLVanessa <vanessaluamba2@icloud.com>
2022-01-19 19:56:59 +0000

    task2

--- session-3/index.js
+++ session-3/index.js
@@ -4,5 +4,16 @@ const a = 5;
 const b = 10;
 let c = a + b;
 
-console.log("original output" + c);
+console.log(c);
+function sayHey() {
+    console.log("Hey!");
+}
+
+sayHey();
+function conversation() {
+    sayHey();
+    console.log("How are you?");
+    console.log("Goodbye");
+}
+conversation();
 
